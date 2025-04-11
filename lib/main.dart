e675616c09132ae3a0a301c813121dd23e3ce3e4
Ebrahim683 <ebrahimrahman20@gmail.com>
2025-04-11 11:21:12 +0600

    [updated] version, landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_drawing_app/pages/drawing/ui/drawing_page.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
   runApp(const MyApp());
 }
 
